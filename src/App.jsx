48adb89f16b8edd733898df1de89521d72201e2d
Gulam Mohammad Khan <gulamkhan512@gmail.com>
2025-07-22 20:00:24 +0530

    build issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import React from "react";
 import Header from "./Components/Header";
-import Navigation from "./Components/Navigation";
 import Hero from "./Components/Hero";
 import Introduction from "./Components/Introduction";
 import Career from "./Components/Career";
